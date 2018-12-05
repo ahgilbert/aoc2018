d6529e30582ab92ff0e3b81dc73c99b51efa433b
Alan Gilbert <alan@imperfectproduce.com>
2018-12-05 08:57:52 -0600

    solve p5

--- src/P05.hs
+++ src/P05.hs
@@ -5,6 +5,7 @@ module P05 where
 import Util
 import Data.Char
 import Data.Either
+import Data.List
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
@@ -12,9 +13,35 @@ p05 :: IO ()
 p05 = do
   input <- slurp 5
   let polymer = fromRight [] $ (runParser parsePolymer "") input
-  let reductions = iterate reduce polymer
-  let final = showPolymer $ findFixed reductions
-  print $ length final
+  let reduced = showPolymer $ reduce polymer
+  print $ length reduced
+  let allChars = nub $ map (toLower . c) polymer
+  let allSeeds = map (\ch -> filter (not . (matches ch)) polymer) allChars
+  let allReductions = map (length . reduce) allSeeds
+  let bestReduction = minimum allReductions
+  print bestReduction
+
+matches :: Char -> Atom -> Bool
+matches ch a = ch == (toLower $ c a)
+
+data Polarity = Up | Down
+  deriving (Show, Eq)
+
+data Atom = Atom { c :: Char, p :: Polarity }
+  deriving (Show, Eq)
+
+reduce :: [Atom] -> [Atom]
+reduce polymer =
+  let reductions = iterate reduce' polymer
+  in findFixed reductions
+
+reduce' :: [Atom] -> [Atom]
+reduce' [] = []
+reduce' (a:[]) = [a]
+reduce' (a:b:cs) =
+  if isCollision a b
+  then reduce' cs
+  else (a:(reduce' (b:cs)))
 
 findFixed :: [[a]] -> [a]
 findFixed [] = []
@@ -23,23 +50,6 @@ findFixed (a:b:cs) =
   if length a == length b
   then a
   else findFixed (b:cs)
-
-data Polarity = Up | Down
-  deriving (Show, Eq)
-
-data Atom = Atom { c :: Char, p :: Polarity }
-  deriving (Show, Eq)
-
-showPolymer :: [Atom] -> String
-showPolymer as = map c as
-
-reduce :: [Atom] -> [Atom]
-reduce [] = []
-reduce (a:[]) = [a]
-reduce (a:b:cs) =
-  if isCollision a b
-  then reduce cs
-  else (a:(reduce (b:cs)))
 
 isCollision :: Atom -> Atom -> Bool
 isCollision a b = (toLower $ c a) == (toLower $ c b) && (p a) /= (p b)
@@ -62,3 +72,6 @@ parseAtom = choice [parseUp, parseDown]
 
 parsePolymer :: Parser [Atom]
 parsePolymer = many parseAtom
+
+showPolymer :: [Atom] -> String
+showPolymer as = map c as
