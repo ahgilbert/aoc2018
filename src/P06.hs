c515908401338995d51a23655fa785c098edb9ca
Alan Gilbert <alan@imperfectproduce.com>
2018-12-06 14:52:34 -0600

    add isBounded predicate

--- src/P06.hs
+++ src/P06.hs
@@ -3,6 +3,7 @@
 module P06 where
 
 import Data.Either
+import Data.List
 import qualified Math.Geometry.Grid as G
 import qualified Math.Geometry.Grid.Square as G
 import Text.Megaparsec
@@ -14,5 +15,13 @@ p06 = do
   input <- lines <$> slurp 6
   let points = rights $ map (runParser parsePoint "") input
       g = G.UnboundedSquareGrid
-  mapM_ print points
-  print $ G.distance g (0,0) (0,1)
+  print $ isBounded (0,0) [(x,y) | x <- [(-1),1], y <- [(-1),1]]
+  print $ isBounded (0,0) [(1,1),(1,2),(-1,4)]
+
+isBounded :: (Int, Int) -> [(Int, Int)] -> Bool
+isBounded p ps =
+  map (\p2 -> G.directionTo G.UnboundedSquareGrid p p2) ps
+  |> concat
+  |> nub
+  |> length
+  |> (== 4)
