200fb1d475bd201a047e261eed4c2673b19bd23b
Alan Gilbert <alan@imperfectproduce.com>
2018-12-06 14:31:47 -0600

    add graph lib

--- src/P06.hs
+++ src/P06.hs
@@ -3,6 +3,8 @@
 module P06 where
 
 import Data.Either
+import qualified Math.Geometry.Grid as G
+import qualified Math.Geometry.Grid.Square as G
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Util
@@ -11,5 +13,6 @@ p06 :: IO ()
 p06 = do
   input <- lines <$> slurp 6
   let points = rights $ map (runParser parsePoint "") input
+      g = G.UnboundedSquareGrid
   mapM_ print points
-
+  print $ G.distance g (0,0) (0,1)
