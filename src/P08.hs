df92237a47f12a76a96793b1eccc5b9c084673e3
Alan Gilbert <alan@imperfectproduce.com>
2018-12-08 08:55:58 -0600

    simpler parser, solve p1, almost solve p2 why does 'map scoretree candidatekids' only score the first kid?

--- src/P08.hs
+++ src/P08.hs
@@ -10,11 +10,17 @@ import Text.Megaparsec.Char
 
 type Memory = Tree [Int]
 
+readInt :: String -> Int
+readInt = read
+
 p08 :: IO ()
 p08 = do
-  input <- lines <$> slurp 8
-  let license = rights $ map (runParser parseMemory "") input
-  print "write some code"
+  input <- map readInt <$> words <$> slurp 108
+  -- let license = fromRight [] $ (runParser parseMemory "") input
+  let (tree, rest) = buildTree input
+  print rest
+  print $ sum $ concat $ flatten tree
+  print $ scoreTree tree
 
 buildTree :: [Int] -> (Memory, [Int])
 buildTree (nKids:nMeta:rest) =
@@ -28,6 +34,19 @@ buildForest n (kids, rest) =
   let (child, rest') = buildTree rest
   in buildForest (n - 1) ((child:kids), rest')
 
+scoreTree :: Memory -> [Int]
+scoreTree t =
+  if (null (subForest t))
+  then ((rootLabel t))
+  else
+    let kids = subForest t
+        idxes = rootLabel t
+                |> filter (\i -> i > 0 && i <= (length kids))
+                |> map (\i -> i - 1)
+        candidateKids = map (kids !!) idxes
+        scores = map scoreTree candidateKids
+    in concat scores
+
 ----------- parsers -------------
 
 parseIntToken :: Parser Int
@@ -37,5 +56,4 @@ parseIntToken = do
   return i
 
 parseMemory :: Parser [Int]
-parseMemory = do
-  many parseIntToken
+parseMemory = many parseIntToken
