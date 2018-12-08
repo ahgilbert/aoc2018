78f3fab9c7f703b35c2c1ddb279e6bae6f952ab4
Alan Gilbert <alan@imperfectproduce.com>
2018-12-08 06:20:15 -0600

    p08 recursive descent compiles

--- src/P08.hs
+++ src/P08.hs
@@ -4,14 +4,29 @@ module P08 where
 
 import Util
 import Data.Either
+import Data.Tree
 import Text.Megaparsec
 import Text.Megaparsec.Char
+
+type Memory = Tree [Int]
 
 p08 :: IO ()
 p08 = do
   input <- lines <$> slurp 8
   let license = rights $ map (runParser parseMemory "") input
   print "write some code"
+
+buildTree :: [Int] -> (Memory, [Int])
+buildTree (nKids:nMeta:rest) =
+  let (kids, remainder) = buildForest nKids ([], rest)
+      metadata = take nMeta remainder
+  in (Node { rootLabel = metadata, subForest = kids }, drop nMeta remainder)
+
+buildForest :: Int -> ([Memory], [Int]) -> ([Memory], [Int])
+buildForest 0 rest = rest
+buildForest n (kids, rest) =
+  let (child, rest') = buildTree rest
+  in buildForest (n - 1) ((child:kids), rest')
 
 ----------- parsers -------------
 
