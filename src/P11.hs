18898baaa3bc4c0ad518d091991b395a64b3f9a3
Alan Gilbert <alan@imperfectproduce.com>
2018-12-11 09:41:27 -0600

    calculate power level

--- src/P11.hs
+++ src/P11.hs
@@ -7,6 +7,17 @@ import System.Environment
 
 p11 :: IO ()
 p11 = do
-  serial <- getArgs
-  print serial
+  serial <- getArgs ||> head ||> read
+  print (0 + serial)
+
+powerLevel :: Int -> Point -> Int
+powerLevel serial (x,y) =
+  let rack = x + 10
+  in ((rack * y) + serial) * rack
+     |> hundreds
+     |> (\i -> i - 5)
+
+hundreds :: Int -> Int -- return hundreds digit
+hundreds n =
+  ((n `mod` 1000) - (n `mod` 100)) `div` 100
 
