078b4415f94ef659494aed37ed0d0be96f3caf4f
Alan Gilbert <alan@imperfectproduce.com>
2018-12-11 09:59:24 -0600

    solve p11.1, peek at 11.2

--- src/P11.hs
+++ src/P11.hs
@@ -3,12 +3,23 @@
 module P11 where
 
 import Util
+import Data.List
 import System.Environment
 
 p11 :: IO ()
 p11 = do
   serial <- getArgs ||> head ||> read
-  print (0 + serial)
+  let candidates = [(x,y) | x <- [1..298], y <- [1..298]]
+      levels = map (\p -> (p, regionPower serial 3 p)) candidates
+      winner = maximumBy (\a b -> compare (snd a) (snd b)) levels
+  print winner
+
+regionPower :: Int -> Int -> Point -> Int -- power level of 3x3 with Point as top left
+regionPower serial size (tlx,tly) =
+  let s = size - 1
+      neighborhood = [(x,y) | x <- [tlx..tlx + s], y <- [tly..tly + s]]
+  in map (powerLevel serial) neighborhood
+     |> sum
 
 powerLevel :: Int -> Point -> Int
 powerLevel serial (x,y) =
