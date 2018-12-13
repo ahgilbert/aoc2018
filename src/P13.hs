4c144a33b8e80d34e4eaec4348d0446e1b3cc81b
Alan Gilbert <alan@imperfectproduce.com>
2018-12-13 09:38:48 -0600

    write a bunch of logic

--- src/P13.hs
+++ src/P13.hs
@@ -4,34 +4,85 @@ module P13 where
 
 import Util
 import qualified Data.Array as A
+import Data.Array ((!))
 import Data.List
 import Data.Maybe
 
 data Turn = Louie | Onward | Righty
-  deriving (Show)
+  deriving (Show, Eq)
 data Direction = N | S | E | W
-  deriving (Show)
-data TrackSegment = Straight Direction | Intersection | Bend | BackBend | Empty
-  deriving (Show)
+  deriving (Show, Eq)
+data TrackSegment = Straight | Intersection | Bend | BackBend | Empty
+  deriving (Show, Eq)
 type Track = A.Array Point TrackSegment
 data Cart = Cart { loc :: Point, dir :: Direction, turns :: [Turn] }
+  deriving (Eq)
 instance Show Cart where
   show c = (show $ loc c) <> " " <> (show $ dir c) <> " " <> (show $ head $ turns c)
 
 p13 :: IO ()
 p13 = do
   input <- map (map readGrid) <$> lines <$> slurp 13
-  let bounds = (length (head input), length input)
+  let bounds = ((0,0),(length (head input), length input))
       input' = input
                |> zip [0..]
                |> map (\(row, cols) -> zip [(col,row) | col <- [0..]] cols)
-      track = map (fmap fst) input'
-              |> A.listArray bounds
+      track = (map . map) (fmap fst) input'
+              |> concat
+              |> A.accumArray (flip const) Empty bounds
       carts = input'
               |> concat
               |> filter (\(_,(_,cart)) -> isJust cart)
               |> map (\(coord,(_,cart)) -> (fromJust cart) { loc = coord })
-  mapM_ print (sortCarts carts)
+      faith = iterate (step track) carts
+      crash = dropWhile (not . hasCrash) faith
+              |> head
+              |> detectCrash
+  print crash
+
+hasCrash :: [Cart] -> Bool
+hasCrash cs = detectCrash cs |> length |> (> 0)
+
+detectCrash :: [Cart] -> [Point]
+detectCrash carts =
+  carts
+  |> map loc
+  |> sort
+  |> group
+  |> filter (\gs -> length gs > 1)
+  |> map head
+
+step :: Track -> [Cart] -> [Cart]
+step track carts =
+  map go carts
+  where
+    go c =
+      let next = move (loc c) (dir c)
+          section = track ! next
+          newDir = case section of
+                     Bend -> Louie
+                     BackBend -> Righty
+                     Intersection -> head (turns c)
+                     Straight -> Onward
+                     Empty -> undefined
+                   |> turn (dir c)
+          turns' = if (section == Intersection) then tail (turns c) else turns c
+      in c { loc = next, dir = newDir, turns = turns' }
+
+move (x,y) N = (x,y - 1)
+move (x,y) S = (x,y + 1)
+move (x,y) E = (x + 1,y)
+move (x,y) W = (x - 1,y)
+
+turn N Louie = W
+turn N Righty = E
+turn S Louie = E
+turn S Righty = W
+turn E Louie = N
+turn E Righty = S
+turn W Louie = S
+turn W Righty = N
+turn d Onward = d
 
 
 sortCarts :: [Cart] -> [Cart]
@@ -45,12 +96,12 @@ sortCarts cs =
   in sortBy sorter cs
 
 readGrid :: Char -> (TrackSegment, Maybe Cart)
-readGrid '-' = (Straight E, Nothing)
-readGrid '<' = (Straight E, Just(newCart W))
-readGrid '>' = (Straight E, Just(newCart E))
-readGrid '|' = (Straight N, Nothing)
-readGrid 'v' = (Straight N, Just(newCart S))
-readGrid '^' = (Straight N, Just(newCart N))
+readGrid '-' = (Straight, Nothing)
+readGrid '<' = (Straight, Just(newCart W))
+readGrid '>' = (Straight, Just(newCart E))
+readGrid '|' = (Straight, Nothing)
+readGrid 'v' = (Straight, Just(newCart S))
+readGrid '^' = (Straight, Just(newCart N))
 readGrid '+' = (Intersection, Nothing)
 readGrid '/' = (Bend, Nothing)
 readGrid '\\' = (BackBend, Nothing)
