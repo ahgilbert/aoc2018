6544a9b50b4e023ac3e918b3c567fe591e09786b
Alan Gilbert <alan@imperfectproduce.com>
2018-12-13 09:38:38 -0600

    build array of carts

--- src/P13.hs
+++ src/P13.hs
@@ -3,6 +3,7 @@
 module P13 where
 
 import Util
+import qualified Data.Array as A
 import Data.List
 import Data.Maybe
 
@@ -10,24 +11,40 @@ data Turn = Louie | Onward | Righty
   deriving (Show)
 data Direction = N | S | E | W
   deriving (Show)
-data Track = Straight Direction | Intersection | Bend | BackBend | Empty
+data TrackSegment = Straight Direction | Intersection | Bend | BackBend | Empty
   deriving (Show)
+type Track = A.Array Point TrackSegment
 data Cart = Cart { loc :: Point, dir :: Direction, turns :: [Turn] }
 instance Show Cart where
   show c = (show $ loc c) <> " " <> (show $ dir c) <> " " <> (show $ head $ turns c)
 
 p13 :: IO ()
 p13 = do
-  input <- map (map readGrid) <$> lines <$> slurp 1013
-  let tracks = map (map fst) input
-      carts = zip [0..] input
-              |> map (\(row, cols) -> zip [(col,row) | col <- [0..]] cols)
+  input <- map (map readGrid) <$> lines <$> slurp 13
+  let bounds = (length (head input), length input)
+      input' = input
+               |> zip [0..]
+               |> map (\(row, cols) -> zip [(col,row) | col <- [0..]] cols)
+      track = map (fmap fst) input'
+              |> A.listArray bounds
+      carts = input'
               |> concat
               |> filter (\(_,(_,cart)) -> isJust cart)
               |> map (\(coord,(_,cart)) -> (fromJust cart) { loc = coord })
-  mapM_ print carts
+  mapM_ print (sortCarts carts)
 
-readGrid :: Char -> (Track, Maybe Cart)
+
+sortCarts :: [Cart] -> [Cart]
+sortCarts cs =
+  let sorter = (\c1 c2 ->
+                 let (x1,y1) = loc c1
+                     (x2,y2) = loc c2
+                     xComp = compare x1 x2
+                     yComp = compare y1 y2
+                 in if xComp == EQ then yComp else xComp)
+  in sortBy sorter cs
+
+readGrid :: Char -> (TrackSegment, Maybe Cart)
 readGrid '-' = (Straight E, Nothing)
 readGrid '<' = (Straight E, Just(newCart W))
 readGrid '>' = (Straight E, Just(newCart E))
