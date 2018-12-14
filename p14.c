e8f9d5575518732a887cce6eb5ae86901a1d1a0e
Alan Gilbert <alan@imperfectproduce.com>
2018-12-14 14:31:34 -0600

    solve p1, work towards p2

--- p14.c
+++ p14.c
@@ -53,26 +53,115 @@ struct Node *moveClockwise(struct Node *current, long steps) {
     return current;
 }
 
-void walkArray(struct Node* current) {
-    struct Node *here = current->next;
-    printf("(%ld) ", current->val);
-    while(here != current) {
-        printf("%ld ", here->val);
+void walkArray(struct Node* current, struct Node* one, struct Node* two) {
+    struct Node *here = current;
+    do {
+        if(here == one) {
+            printf("(%ld) ", here->val);
+        } else if(here == two) {
+            printf("[%ld] ", here->val);
+        } else {
+            printf(" %ld  ", here->val);
+        }
         here = here->next;
+    } while(here != current);
+    printf("\n");
+}
+
+int numDigits(long n) {
+    int d = 1;
+    while (n >= 10) {
+        d++;
+        n /= 10;
+    }
+    return d;
+}
+
+struct Node *elfStep(struct Node *elf) {
+    long score = 1 + elf->val;
+    return moveClockwise(elf, score);
+}
+
+long scoreRecipes(struct Node *elves[], int nElves){
+    long s = 0;
+    for (int i = 0; i < nElves; i++) {
+        s += elves[i]->val;
+    }
+    return s;
+}
+
+void part1(struct Node *origin, int skip) {
+    for(int i = 0; i < skip; i++){
+        origin = origin->next;
+    }
+    printf("part 1: ");
+    for(int i = 0; i < 10; i++){
+        printf("%ld", origin->val);
+        origin = origin->next;
     }
     printf("\n");
 }
 
+int matchGo(struct Node *origin){
+    // int target[] = {1,0,5,4,7,0}; int d = 6; // {0,7,4,5,0,1};
+    // int target[] = {9,8,5,1,5}; int d = 5; // expect 9
+    // int target[] = {5,4,2,1,0}; int d = 5; // expect 5
+    int target[] = {0,1,5,2,9}; int d = 5; // expect 18
+    // int target[] = {4,1,4,9,5}; int d = 5; // expect 2018
+    // int target[] = {9};
+    struct Node *current = origin;
+    for(int i = 0; i < d; i++) {
+        if(current->val != target[i]){
+            // printf("x");
+            return 0;
+        }
+        current = current->prev;
+    }
+    return 1;
+}
+
+int match(struct Node *origin){
+    if(matchGo(origin->prev)) {
+        return 1;
+    }
+
+    return matchGo(origin->prev->prev);
+}
+
 int main(int argc, char *argv[]){
+    // input: 074501
     long stop = atoi(argv[1]);
     struct Node *origin = mkRing(3);
     insert(origin, 7);
+    int numElves = 2;
     struct Node* elves[2] = {origin,(origin->next)};
 
-    for(long m=1; m < stop + 10; m++) {
-        for(long n=0; n < 2; n++) {
-            printf("elf %ld: %ld\n", n+1, elves[n]->val);
+    int m = 0;
+    while(!match(origin)) {
+        long score = scoreRecipes(elves, 2);
+
+        // break score into its digits
+        int nDig = numDigits(score);
+        long nn = score;
+        int digs[nDig];
+        for(int i = 1; i <= nDig; i++){
+            digs[nDig - i] = nn % 10;
+            nn /= 10;
         }
+
+        for(int i = 0; i < nDig; i++){
+            insert(origin->prev, digs[i]);
+        }
+
+        for(int i = 0; i < numElves; i++) {
+            elves[i] = elfStep(elves[i]);
+        }
+        printf("%d:\t", m);
+        walkArray(origin, elves[0], elves[1]);
+        m++;
     }
+
+    printf("part 2: %d", m - 1);
+
     return 0;
 }
