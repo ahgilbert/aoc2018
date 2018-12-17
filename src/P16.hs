97b9c33d6cab178d73e2f333f358953a0ff65ec9
Alan Gilbert <alan@imperfectproduce.com>
2018-12-16 21:35:33 -0600

    separate opcode from args

--- src/P16.hs
+++ src/P16.hs
@@ -5,11 +5,13 @@ module P16 where
 import Util
 import Data.Bits ((.&.),(.|.))
 import Data.Either
+import Data.List
+import qualified Data.Map.Strict as M
 import qualified Data.Array.Unboxed as A
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
-data Sample = Sample { before :: [Int], after :: [Int], instruction :: (Int, Int, Int, Int) }
+data Sample = Sample { before :: [Int], after :: [Int], opcode :: Int, instruction :: (Int, Int, Int) }
   deriving (Eq, Show)
 
 p16 :: IO ()
@@ -17,8 +19,22 @@ p16 = do
   input <- slurp 16
   let samples = fromRight [] $ runParser parseSamples "" input
       results = map testSample samples
-                |> filter (\os -> length os >= 3)
-  print $ length results
+      byOpcode = samples
+                 |> sortBy (\s1 s2 -> compare (opcode s1) (opcode s2))
+                 |> groupBy (\s1 s2 -> (opcode s1) == (opcode s2))
+  putStrLn $ "part 1: " <> (show $ length $ filter (\os -> length os >= 3) results)
+  print $ length byOpcode
+
+{-
+testOpcode (code, ss) = -- given a set of samples, see which instructions are true for all samples
+  instructions
+  |> zip [0..]
+  |> fmap (\i -> all (i (before s) (instructions s) == after s) ss)
+  |> filter snd
+  |> map fst
+-}
+
+testSample2 i s = (i (before s) (instruction s)) == (after s)
 
 testSample s =
   map (\i -> i (before s) (instruction s)) instructions
@@ -31,29 +47,30 @@ splice regs reg val =
 
 -------------- instructions -------------------
 instructions = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+instsByPos = zip [0..] instructions |> M.fromList
 
-addr regs (_,a,b,c) = (regs !! a) + (regs !! b) |> splice regs c
-addi regs (_,a,b,c) = (regs !! a) + b |> splice regs c
+addr regs (a,b,c) = (regs !! a) + (regs !! b) |> splice regs c
+addi regs (a,b,c) = (regs !! a) + b |> splice regs c
 
-mulr regs (_,a,b,c) = (regs !! a) * (regs !! b) |> splice regs c
-muli regs (_,a,b,c) = (regs !! a) * b |> splice regs c
+mulr regs (a,b,c) = (regs !! a) * (regs !! b) |> splice regs c
+muli regs (a,b,c) = (regs !! a) * b |> splice regs c
 
-banr regs (_,a,b,c) = (regs !! a) .&. (regs !! b) |> splice regs c
-bani regs (_,a,b,c) = (regs !! a) .&. b |> splice regs c
+banr regs (a,b,c) = (regs !! a) .&. (regs !! b) |> splice regs c
+bani regs (a,b,c) = (regs !! a) .&. b |> splice regs c
 
-borr regs (_,a,b,c) = (regs !! a) .|. (regs !! b) |> splice regs c
-bori regs (_,a,b,c) = (regs !! a) .|. b |> splice regs c
+borr regs (a,b,c) = (regs !! a) .|. (regs !! b) |> splice regs c
+bori regs (a,b,c) = (regs !! a) .|. b |> splice regs c
 
-setr regs (_,a,_,c) = (regs !! a) |> splice regs c
-seti regs (_,a,_,c) = a |> splice regs c
+setr regs (a,_,c) = (regs !! a) |> splice regs c
+seti regs (a,_,c) = a |> splice regs c
 
-gtir regs (_,a,b,c) = (if a > (regs !! b) then 1 else 0) |> splice regs c
-gtri regs (_,a,b,c) = (if (regs !! a) > b then 1 else 0) |> splice regs c
-gtrr regs (_,a,b,c) = (if (regs !! a) > (regs !! b) then 1 else 0) |> splice regs c
+gtir regs (a,b,c) = (if a > (regs !! b) then 1 else 0) |> splice regs c
+gtri regs (a,b,c) = (if (regs !! a) > b then 1 else 0) |> splice regs c
+gtrr regs (a,b,c) = (if (regs !! a) > (regs !! b) then 1 else 0) |> splice regs c
 
-eqir regs (_,a,b,c) = (if a == (regs !! b) then 1 else 0) |> splice regs c
-eqri regs (_,a,b,c) = (if (regs !! a) == b then 1 else 0) |> splice regs c
-eqrr regs (_,a,b,c) = (if (regs !! a) == (regs !! b) then 1 else 0) |> splice regs c
+eqir regs (a,b,c) = (if a == (regs !! b) then 1 else 0) |> splice regs c
+eqri regs (a,b,c) = (if (regs !! a) == b then 1 else 0) |> splice regs c
+eqrr regs (a,b,c) = (if (regs !! a) == (regs !! b) then 1 else 0) |> splice regs c
 
 ------------ parsers ---------------
 parseSample :: Parser Sample
@@ -70,7 +87,7 @@ parseSample = do
   newline
   string "After: "
   after <- read <$> manyTill asciiChar newline
-  return $ Sample { before = before, after = after, instruction = (i,a,b,c) }
+  return $ Sample { before = before, after = after, opcode = i, instruction = (a,b,c) }
 
 parseSamples :: Parser [Sample]
 parseSamples = endBy parseSample (optional newline)
