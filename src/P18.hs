402b2337c006cba17afe0db78f2bb1bc5bdee923
Alan Gilbert <alan@imperfectproduce.com>
2018-12-18 09:05:06 -0600

    sovle 18.2

--- src/P18.hs
+++ src/P18.hs
@@ -7,12 +7,70 @@ import qualified Data.Array.Unboxed as A
 
 type Settlers = A.Array Point Industry
 data Industry = Open | Wooded | Lumberyard
-  deriving (Eq, Show)
+  deriving (Eq)
+instance Show Industry where
+  show Open = "."
+  show Wooded = "|"
+  show Lumberyard = "#"
 
 p18 :: IO ()
 p18 = do
   settlers <- parseSettlers <$> lines <$> slurp 18
-  mapM_ print settlers
+  let gens = iterate evolve settlers
+      spaced = map (\i -> (i, gens !! i)) [1000,2000..]
+  mapM_ print $ (map . fmap) scoreSettlers (take 50 spaced)
+
+scoreSettlers :: Settlers -> Int
+scoreSettlers s =
+  let land = A.elems s
+      numWooded = land
+                  |> filter (== Wooded)
+                  |> length
+      numLumber = land
+                  |> filter (== Lumberyard)
+                  |> length
+  in numWooded * numLumber
+
+evolve :: Settlers -> Settlers
+evolve s = map (step s) (A.indices s)
+           |> A.listArray (A.bounds s)
+
+step :: Settlers -> Point -> Industry
+step sett p =
+  let here = sett A.! p
+      neighbors = neighbors8 p
+      neighborhood = map (getSett sett) neighbors
+  in grow here neighborhood
+
+grow :: Industry -> [Industry] -> Industry
+grow Open ns =
+  let nTrees = ns |> filter (== Wooded) |> length
+  in if nTrees >= 3 then Wooded else Open
+grow Wooded ns =
+  let nYards = ns |> filter (== Lumberyard) |> length
+  in if nYards >= 3 then Lumberyard else Wooded
+grow Lumberyard ns =
+  let hasTrees = ns |> filter (== Wooded) |> length |> (>= 1)
+      hasLumber = ns |> filter (== Lumberyard) |> length |> (>= 1)
+  in if hasTrees && hasLumber then Lumberyard else Open
+
+------------ utils -----------------
+
+getSett :: Settlers -> Point -> Industry
+getSett sett p =
+  let ((xmin,ymin),(xmax,ymax)) = A.bounds sett
+      inbounds = xmin <= fst p && xmax >= fst p && ymin <= snd p && ymax >= snd p
+  in if inbounds
+     then sett A.! p
+     else Open
+
+printSettlers = (mapM_ putStrLn) . showSettlers
+
+showSettlers :: Settlers -> [String]
+showSettlers s =
+  let ((xmin,ymin),(xmax,ymax)) = A.bounds s
+      rows = map (\y -> [(y,x) | x <- [xmin..xmax]]) [ymin..ymax]
+  in map (\r -> concat $ map (\p -> show $ s A.! p) r) rows
 
 ------------ parsers ---------------
 
