8a1c01ece5c01166024132652c024f2207e51cae
Alan Gilbert <alan@imperfectproduce.com>
2019-01-07 22:34:17 -0600

    some work towards p22

--- src/P22.hs
+++ src/P22.hs
@@ -10,7 +10,7 @@ import qualified Data.Map.Strict as M
 -- depth = 8787
 -- target = (10,725)
 
-type ModeMap = M.Map Point Int
+type ModeMap = M.Map Point Integer
 
 p22 :: IO ()
 p22 = print $ p22' 8727 (10,725)
@@ -18,19 +18,26 @@ p22 = print $ p22' 8727 (10,725)
 p22' :: Int -> (Int, Int) -> ModeMap
 p22' depth target@(xx,yy) =
   let northEdge = [(x,0) | x <- [0..xx]]
-      north = map (\p@(x,_) -> (p,(x * 16807) `mod` 20183)) northEdge
+      north = map (\p@(x,_) -> (p,gi target p)) northEdge
       m = M.fromList north
   in foldl (folder depth target xx) m [1..yy]
 
 folder :: Int -> Point -> Int -> ModeMap -> Int -> ModeMap
-folder d t w m y = foldl (erosionLevel d t) m [(x,y) | x <- [0..w]]
+folder d t w m y = foldl (geoIndex t) m [(x,y) | x <- [0..w]]
 
-erosionLevel :: Int -> Point -> ModeMap -> Point -> ModeMap
-erosionLevel depth target m p@(x,y)
+geoIndex :: Point -> ModeMap -> Point -> ModeMap
+geoIndex target m p@(x,y)
   | p == target = M.insert p 0 m
-  | x == 0 || y == 0 = M.insert p (el depth target p) m
-  | True = M.insert p ((grab (x-1,y) * grab (x,y-1)) `mod` 20183) m
+  | x == 0 || y == 0 = M.insert p (gi target p) m
+  | True = M.insert p (grab (x-1,y) * grab (x,y-1)) m
   where grab p = fromJust $ M.lookup p m
+
+gi :: Point -> Point -> Integer
+gi t p@(x,y)
+  | p == t = 0
+  | x == 0 = (toInteger y * 48271)
+  | y == 0 = (toInteger x * 16807)
+  | True   = undefined
 
 drawCave :: ModeMap -> [String]
 drawCave m =
@@ -45,10 +52,3 @@ drawSpot 0 = '.'
 drawSpot 1 = '='
 drawSpot 2 = '|'
 drawSpot _ = 'X'
-
-el :: Int -> Point -> Point -> Int
-el d t p@(x,y)
-  | p == t = 0
-  | x == 0 = (d + y * 48271)
-  | y == 0 = (d + x * 16807)
-  | True   = undefined
